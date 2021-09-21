35d6413bea006ccebf22100a4747379991c7b777
ahmadalasaad <ahmadalserese9754@gnail.com>
2021-09-21 11:15:22 +0300

    signup and user admin page

--- src/components/LoginSignUp.js
+++ src/components/LoginSignUp.js
@@ -1,44 +1,71 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import Dropdown from 'react-bootstrap/Dropdown'
+// import Dropdown from 'react-bootstrap/Dropdown'
 
 class LoginSignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            useresdata:[],
         }
     }
+    ////////////////////////////////////Add user by signup form/////////////////////////////////////
 
+    handelSubmitSignIn = (e) => {
+        e.preventDefault();
+        console.log(e.target.userName);
+        const reqBody = {
+            userName: e.target.userName.value,
+            password: e.target.userPassword.value,
+            email: e.target.userEmail.value,
+            phoneNo:e.target.phonNum.value,
+            privilege:'',
+            type: e.target.userType.value,
+            busNoforTeacherOnly:'',
+        }
+        axios.post(`${process.env.REACT_APP_API_URL}/user`, reqBody).then(createduserObject => {
+            this.state.useresdata.push(createduserObject.data);
+            this.setState({ useresdata: this.state.useresdata });
+        }).catch(() => alert("Something went wrong!"));
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    componentDidMount = () => {
+        axios.get(`${process.env.REACT_APP_API_URL}/user`).then((userResponse) => {
+
+            this.setState({ useresdata: userResponse.data });
+        }).catch(error => alert(error.message));
+
+    }
     render() {
         return (
             <div>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form onSubmit={this.handelSubmitSignIn}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail"  >
                         <Form.Label>User Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter your name" />
+                        <Form.Control type="text" placeholder="Enter your name" name="userName"/>
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Group className="mb-3" controlId="formBasicEmail" >
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email"name="userEmail" />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Group className="mb-3" controlId="formBasicPassword" >
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" name="userPassword"/>
                     </Form.Group>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Select
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Admin</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Parent</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Teacher</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
+                    <Form.Group className="mb-3" controlId="formBasicEmail" >
+                        <Form.Label>Phon NUmber</Form.Label>
+                        <Form.Control type="text" placeholder="Enter your phone number" name="phonNum" />
+                    </Form.Group>
+                    <Form.Control as="select" name="userType" >
+                  <option value='Admin'>Admin</option>
+                  <option value='Parent'>Parent</option>
+                  <option value='Teacher'>Teacher</option>
+                   </Form.Control>
                     <Button variant="primary" type="submit">
                         Sign Up
                     </Button>
@@ -49,4 +76,3 @@ class LoginSignUp extends Component {
     }
 }
 export default LoginSignUp;
-
