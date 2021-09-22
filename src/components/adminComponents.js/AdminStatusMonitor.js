83f9d923c83f4d993a7c821b2149a452e4031bd0
osama900 <osamaos2233@gmail.com>
2021-09-22 17:57:11 +0300

    added some css

--- src/components/adminComponents.js/AdminStatusMonitor.js
+++ src/components/adminComponents.js/AdminStatusMonitor.js
@@ -2,13 +2,14 @@
 // this feature created to show information about the busses in the Admin control panel
 import React from "react";
 import Card from "react-bootstrap/Card";
+import Table from 'react-bootstrap/Table'
 
-import ListGroup from "react-bootstrap/ListGroup";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from "react-bootstrap/Row"
 
-
-
+import './AdminStatusMonitor.css'
 import axios from "axios";
+
 
 class AdminStatusMonitor extends React.Component {
   constructor(props) {
@@ -21,9 +22,7 @@ class AdminStatusMonitor extends React.Component {
     };
   }
 
-  handleClickOnEdit = () => {
-    ////do something when edit button clicked
-  };
+
   componentDidMount = () => {
 
     // bus info data
@@ -56,81 +55,107 @@ class AdminStatusMonitor extends React.Component {
 
   render() {
     return (
-      <>
-        {console.log(this.state.busData)}
-        {console.log(this.state.userData)}
-        {console.log(this.state.studentData)}
+      <div id="mainDivCards">
 
-        {this.state.busData.length > 0 &&
-          this.state.busData.map((bus) => {
-            return (
-              <>
-                <div>
-                  <Card style={{ width: "18rem" }} >
-                    <Card.Body id="card">
-                      <Card.Title>
-                        <h4>
 
-                          {"bus No : " + bus.busNo} <br />
-                          {"Driver Name : " + bus.driverName} <br />
-                          {this.state.userData.map(teacher => {
+        <Row md="3" id="RowCards">
+          <>
 
-                            if (teacher.busNoforTeacherOnly === bus.busNo) {
-                              return "Teacher Name : " + teacher.userName;
+            {this.state.busData.length > 0 &&
+              this.state.busData.map((bus) => {
+                return (
+                  <>
+
+                    <Card id="bCard" style={{ width: "18rem" }} >
+                      <Card.Body id="card" >
+                        <Card.Title >
+                          <h4 class="cardTitle">
+                            {"Bus Number : " + bus.busNo}
+                          </h4>
+                          <h4 class="cardTitle">
+                            {"Driver Name : " + bus.driverName}
+                          </h4>
+                          <h4 class="cardTitle">
+                            {this.state.userData.map(teacher => {
+
+                              if (teacher.privilege === "2") {
+                                if (teacher.busNoforTeacherOnly === bus.busNo) {
+                                  return "Teacher Name : " + teacher.userName;
+                                }
+                              }
+
+
                             }
 
+                            )}
+                          </h4>
+                        </Card.Title>
 
+                        {/* filter student which has the buss number = this.bus.busNumber */}
+                        {this.state.studentData.map(student => {
+                          if (student.busNo === bus.busNo) {
+                            return (
+                              // <div>
+                              //   {/* {console.log(student.busNo)} */}
+                              //   <ListGroup>
+                              //     <ListGroup.Item id="CardStdItem">
+                              //       <span>{student.studentName}</span>
+                              //       {student.status === "1" && <span>    Away</span>}
+                              //       {student.status === "2" && <span>     Almost there</span>}
+                              //       {student.status === "3" && <span>     Arrived</span>}
+                              //       {/* status: {students.status} */}
+
+                              //     </ListGroup.Item>
+                              //   </ListGroup>
+
+                              // </div>
+
+                              <div>
+                                <Table striped bordered hover>
+
+                                  <tbody>
+                                    <tr>
+                                      <td id="tableStdRow"> <span>{student.studentName}</span>
+                                        {student.status === "1" && <span> 🔴    Away</span>}
+                                        {student.status === "2" && <span>  🟠    Almost there</span>}
+                                        {student.status === "3" && <span>  🟢    Arrived</span>}</td>
+
+                                    </tr>
+                                  </tbody>
+                                </Table>
+                              </div>
+                            );
                           }
-
-                          )}
-                        </h4>
-                      </Card.Title>
-
-                      {/* filter student which has the buss number = this.bus.busNumber */}
-                      {this.state.studentData.map(student => {
-                        if (student.busNo === bus.busNo) {
-                          return (
-                            <div>
-                              {/* {console.log(student.busNo)} */}
-                              <ListGroup>
-                                <ListGroup.Item>
-                                  <span>{student.studentName}</span>
-                                  {student.status === "1" && <span>    Away</span>}
-                                  {student.status === "2" && <span>     Almost there</span>}
-                                  {student.status === "3" && <span>     Arrived</span>}
-                                  {/* status: {students.status} */}
-
-                                </ListGroup.Item>
-                              </ListGroup>
-                            </div>
-                          );
-                        }
-                      })}
+                        })}
 
 
-                    </Card.Body>
-                  </Card>
-                </div>
-              </>
-            );
-          })}
-        <div>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              width={200}
-              height={200}
-              src="https://image.pngaaa.com/73/4388073-middle.png"
-            />
-            <Card.Body>
-              <Card.Text>ADD NEW</Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-      </>
+                      </Card.Body>
+                    </Card>
+
+                  </>
+
+                );
+              })}
+            {/* <div>
+
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                height={180}
+                width={130}
+                src={imgPlus} />
+              <Card.Body>
+                <h4 id="AddTxtCard" >ADD NEW</h4>
+              </Card.Body>
+            </Card>
+          </div> */}
+          </>
+        </Row>
+      </div>
+
     );
   }
-  s;
+  ;
 }
 
 export default AdminStatusMonitor;
